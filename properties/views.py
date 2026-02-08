b0672fc4047071aff90ea24c55d524a5740e38ea
Emmanuel <emmanuelkibe148@gmail.com>
2026-02-08 16:34:50 +0300

    feat: convert property list to cached JsonResponse

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
 
-@cache_page(60 * 15)  # Cache the result for 15 minutes (900 seconds)
+@cache_page(60 * 15)
 def property_list(request):
+    # Fetch all properties
     properties = Property.objects.all()
-    return render(request, 'properties/property_list.html', {'properties': properties})
+    
+    # Prepare data for JsonResponse
+    data = [
+        {
+            "id": p.id,
+            "title": p.title,
+            "description": p.description,
+            "price": str(p.price),
+            "location": p.location,
+            "created_at": p.created_at.isoformat(),
+        }
+        for p in properties
+    ]
+    
+    return JsonResponse({"data": data}, safe=False)
